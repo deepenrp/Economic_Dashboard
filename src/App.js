d376721b91ddf70eca4331acf3994cf0ddb3b9f2
unknown <deepenrp@yahoo.com>
2021-03-28 21:16:45 -0400

    Start React Project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src='./images/real_estate.jpg' alt="Real Estate" width="1100px" height="350px" />
+        <p>
+          Economic DashBoard
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
