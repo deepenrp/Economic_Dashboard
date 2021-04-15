6e2865a2d546895e61cfe2fab33fe293b36cec65
unknown <deepenrp@yahoo.com>
2021-04-14 21:21:19 -0400

    Dependencies Installed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import './css/App.css';
+import real_estate_image from './images/real_estate.jpg';
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <img src='./images/real_estate.jpg' alt="Real Estate" width="1100px" height="350px" />
+        <img src={real_estate_image} alt="Real Estate" width="1100px" height="350px" />
         <p>
-          Economic DashBoard
+          Economic Dashboard
         </p>
       </div>
     </div>
